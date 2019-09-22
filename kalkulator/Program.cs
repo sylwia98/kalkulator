043eae725120cc71ba9dc05855506c62dae09622
sylwia98 <syla.111@wp.pl>
2019-09-22 15:35:33 +0200

    Introduce menu to application

--- kalkulator/Program.cs
+++ kalkulator/Program.cs
@@ -10,6 +10,17 @@ namespace kalkulator
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Dodawanie");
+                Console.WriteLine("2. Odejmowanie");
+                int menuOption = Convert.ToInt32(Console.ReadLine());
+                if (menuOption == 0)
+                {
+                    break;
+                }
+            }
         }
     }
 }
